aa40e1382d01fe382d1994c6b4046606c76cb537
nelsonelaye <nelsonelaye@gmail.com>
2022-09-27 12:44:01 +0100

    error check

--- config/mysql.js
+++ config/mysql.js
@@ -8,6 +8,14 @@ const mysqlConnection = mysql.createConnection({
   database: "MondeDB",
 });
 
+var del = mysqlConnection._protocol._delegateError;
+mysqlConnection._protocol._delegateError = function (err, sequence) {
+  if (err.fatal) {
+    console.trace("fatal error: " + err.message);
+  }
+  return del.call(this, err, sequence);
+};
+
 const sql = "";
 mysqlConnection.connect((err, result) => {
   if (err) {
